36863c04df276157a9c14a61511c938968ae33d0
grigilo <tsoy.grigoriy@gmail.com>
2024-07-09 12:26:08 +0300

    25.2 Task 2

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,7 +1,8 @@
+from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
-from lms.models import Course, Lesson
+from lms.models import Course, Lesson, Subscribe
 from lms.validators import LinkValidator
 
 
@@ -15,6 +16,7 @@ class LessonSerializer(ModelSerializer):
 class CourseSerializer(ModelSerializer):
     lesson_count = SerializerMethodField()
     lesson = LessonSerializer(source="lesson_set", read_only=True, many=True)
+    subscribe = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
@@ -24,3 +26,17 @@ class CourseSerializer(ModelSerializer):
         if object.lesson_set.count():
             return object.lesson_set.count()
         return 0
+
+    def get_subscribe(self, instance):
+        request = self.context.get('request')
+        user = None
+
+        if request:
+            user = request.user
+        return instance.subscribe_set.filter(user=user).exists()
+
+
+class SubscribeSerializer(ModelSerializer):
+    class Meta:
+        model = Subscribe
+        fields = '__all__'
--- lms/views.py
+++ lms/views.py
@@ -1,15 +1,19 @@
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import (
     CreateAPIView,
     DestroyAPIView,
     ListAPIView,
     UpdateAPIView,
-    RetrieveAPIView,
+    RetrieveAPIView, get_object_or_404,
 )
-from lms.models import Course, Lesson
+from lms.models import Course, Lesson, Subscribe
 from lms.permissions import IsModerator, IsOwner
-from lms.serializers import CourseSerializer, LessonSerializer
+from lms.serializers import CourseSerializer, LessonSerializer, \
+    SubscribeSerializer
 
 
 class CourseViewSet(ModelViewSet):
@@ -76,3 +80,24 @@ class LessonDestroyAPIView(DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
     permission_classes = [~IsModerator | IsOwner]
+
+
+class SubscribeCreateAPIView(CreateAPIView):
+    serializer_class = SubscribeSerializer
+
+    def post(self, *args, **kwargs):
+        user = self.request.user
+        course_id = self.request.data.get("course")
+        course_item = get_object_or_404(Course, pk=course_id)
+
+        subscribe, created = Subscribe.objects.get_or_create(user=user,
+                                                             course=course_item)
+        if not created:
+            subscribe.delete()
+            message = 'Subscribe deleted successfully'
+
+        else:
+            message = 'Subscribe created successfully'
+
+        return Response({'message': message})
+
