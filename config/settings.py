a3ab3b08e7c3dbfdd994d0f10d6a866121253067
grigilo <tsoy.grigoriy@gmail.com>
2024-07-15 15:22:40 +0300

    26.2 Done

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "django_celery_beat",
     "drf_yasg",
     "users",
     "lms",
@@ -124,3 +125,22 @@ SIMPLE_JWT = {
 }
 
 STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
+
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
+CELERY_BACKEND_URL = 'redis://127.0.0.1:6379/0'
+CELERY_BEAT_SCHEDULE = {
+    'send reminder': {
+        'task': 'user.tasks.check_last_login',
+        'schedule': timedelta(seconds=10),
+    },
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- lms/views.py
+++ lms/views.py
@@ -14,7 +14,9 @@ from rest_framework.generics import (
 from lms.models import Course, Lesson, Subscribe
 from lms.paginators import LmsPaginator
 from lms.permissions import IsModerator, IsOwner
-from lms.serializers import CourseSerializer, LessonSerializer, SubscribeSerializer
+from lms.serializers import CourseSerializer, LessonSerializer, \
+    SubscribeSerializer
+from lms.tasks import send_mail_about_course
 
 
 class CourseViewSet(ModelViewSet):
@@ -59,6 +61,7 @@ class LessonCreateAPIView(CreateAPIView):
     def perform_create(self, serializer):
         lesson = serializer.save()
         lesson.owner = self.request.user
+        send_mail_about_course.delay(lesson.course.pk)
         lesson.save()
 
 
